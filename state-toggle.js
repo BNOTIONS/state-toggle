1a437a000917a33b9bfaadd77840f00bf5cea6b0
Mark Reale <mark@reale.ca>
2015-02-04 15:15:15 -0500

    Initial commit of state-toggle tool

---
+++ state-toggle.js
@@ -0,0 +1,54 @@
+$(function() {
+  $('[st-role="trigger"]').on('click',function(event){  
+    event.stopPropagation();
+    var $this = $(this),
+    $scope = $this.parents('[st-contain]').length ? $this.parents('[st-contain]') : $(document),
+    whichGroup = $this.attr('st-group'),
+    $whichTriggers = $('[st-role="trigger"][st-group="' + whichGroup + '"]'),
+    whichState = $this.attr('st-state'),
+    stateRemove = $this.attr('st-remove'),
+    isReady = $this.is('[st-ready]'),
+    isRemove = $this.is('[st-remove]'),
+    isRestricted = $this.is('[st-restrict]'),
+    $toggleTarget = $scope.find('[st-role="target"][st-group="' + whichGroup + '"]'), 
+    $removeTarget = $scope.find(stateRemove ? '.' + stateRemove.replace(" ", ".") : "" + '[st-role="target"]'),
+    $inactiveTriggers = $scope.find('[st-remove="' + stateRemove + '"][st-role="trigger"]');
+
+    if((isReady && ($this.attr('st-ready') === 'true')) || !isReady ){
+        if(isRestricted){
+          if($this.attr('st-restrict') === 'off'){
+            $toggleTarget.removeClass(whichState);
+            $whichTriggers.removeClass('st-activated');
+          }
+          else if($this.attr('st-restrict') === 'on'){
+            $toggleTarget.addClass(whichState);
+            $whichTriggers.addClass('st-activated');
+          }
+        }
+        else {
+          if(!$this.hasClass('st-activated')){
+            $inactiveTriggers.removeClass('st-activated');
+            $this.addClass('st-activated');
+          }
+          else{
+            $this.removeClass('st-activated');
+            if(isRemove){
+              $whichTriggers.addClass('st-activated');
+            }
+          }
+          $removeTarget.removeClass(stateRemove);
+          $toggleTarget.toggleClass(whichState);  
+        }
+      
+    }
+  });
+
+  $('[st-role="activate"]').on('click',function(){
+    var whichToggle = $(this).attr('st-group');
+    $('[st-role="trigger"][st-group="' + whichToggle + '"]').attr('st-ready','true');
+  });
+
+  $('[st-role="target"]').on('click',function(event){
+    event.stopPropagation();
+  });
+});
